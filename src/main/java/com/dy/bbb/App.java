33bb7735a53fe708829bf6eb7b1eda02b1355101
bruceshr <bruceshr@163.com>
2019-06-12 16:08:00 +0800

    aa

--- src/main/java/com/dy/bbb/App.java
+++ src/main/java/com/dy/bbb/App.java
@@ -5,11 +5,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "asas" );
-         System.out.println( "sss" );
-        System.out.println("111111");
-        System.out.println("333333");
-        System.out.println("222222");
-        System.out.println("55555");
-        System.out.println("444444");
     }
 }
