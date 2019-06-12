97503af74a4306beb4fabcdb578ad04e6d7c8c45
bruceshr <bruceshr@163.com>
2019-06-12 15:35:46 +0800

    一

--- src/main/java/com/dy/bbb/App.java
+++ src/main/java/com/dy/bbb/App.java
@@ -4,12 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "asas" );
-        System.out.println( "sss" );
-        System.out.println("1111");
-        System.out.println( "sss" );
         System.out.println("111111");
-        System.out.println("4444444");
-        System.out.println("2222222");
     }
 }
