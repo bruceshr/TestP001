809b90d8452e2fe1dbf751c8e8a7f01d83cccd4e
bruceshr <bruceshr@163.com>
2019-06-12 11:34:50 +0800

    第二次提交

--- src/main/java/com/dy/bbb/App.java
+++ src/main/java/com/dy/bbb/App.java
@@ -1,14 +1,12 @@
 package com.dy.bbb;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "asas" );
-         System.out.println( "sss" );
+        System.out.println( "sss" );
+        System.out.println("111111");
+        
     }
 }
